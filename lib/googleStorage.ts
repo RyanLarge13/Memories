bdfc14df2be84c1dc1ffaf5a25af305ed8268793
RyanLarge13 <ryanlarge13@gmail.com>
2024-08-17 10:40:27 -0400

    Creaating Enviornemnt Specific Creds
    Now the storage bucket information for connection
    can be accessed simply by vercel and a simple .env variable

--- lib/googleStorage.ts
+++ lib/googleStorage.ts
@@ -16,10 +16,30 @@ if (mode === "prod") {
 if (!credentials) {
   throw new Error("Could not access storage bucket credentials");
 }
+let storage;
 
-const storage = new Storage({
-  keyFilename: path.join(process.cwd(), credentials),
-});
+if (mode === "dev") {
+  storage = new Storage({
+    keyFilename: path.join(process.cwd(), credentials),
+  });
+}
+if (mode === "prod") {
+  const parsedCredentials = JSON.parse(credentials);
+  if (!parsedCredentials) {
+    throw new Error(
+      "We could not parse the environment variables to connect to google storage bucket"
+    );
+  }
+  storage = new Storage({
+    credentials: parsedCredentials,
+  });
+}
+
+if (!storage) {
+  throw new Error(
+    "We could not initialize your storage bucket with the given credentials"
+  );
+}
 
 const bucketName = "memory-images-bucket";
 const bucket = storage.bucket(bucketName);
